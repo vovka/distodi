0942cdb0592c73bca082f1eb7cb8883fda00f6cd
Dmitry Chopey <d-ima@hotmail.com>
2016-07-18 22:10:46 +0300

    Add base columns to users

---
+++ db/migrate/20160718190525_add_base_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddBaseColumnsToUsers < ActiveRecord::Migration
+  def change
+  	add_column :users, :country, :string
+  	add_column :users, :city, :string
+  	add_column :users, :street, :string
+  	add_column :users, :postal_code, :string
+  	add_column :users, :notice, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718190135) do
+ActiveRecord::Schema.define(version: 20160718190525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,11 @@ ActiveRecord::Schema.define(version: 20160718190135) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "country"
+    t.string   "city"
+    t.string   "street"
+    t.string   "postal_code"
+    t.string   "notice"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
