3b647aa5ab9690b8bb61aa834bc2d0bf3ac6f314
Dmitry Chopey <d-ima@hotmail.com>
2016-07-18 22:32:44 +0300

    Add devise strong params to user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
