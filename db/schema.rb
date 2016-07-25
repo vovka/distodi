908ffd370fbb475eb825ff187846501fcbda1b5f
Dmitry Chopey <d-ima-13@hotmail.com>
2016-07-25 16:39:19 +0300

    Add associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160724192539) do
+ActiveRecord::Schema.define(version: 20160725133543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attribute_kinds", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
