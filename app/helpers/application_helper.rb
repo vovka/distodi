c81d4757374ea5fcc736bb543db9f6686258476a
Dmitry Chopey <dmitry.chopey@gmail.com>
2016-08-27 21:19:29 +0300

    Fix my_panel link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
     if user_signed_in?
       link_to "My panel (#{current_user.email})", user_path(current_user)
     elsif company_signed_in?
-      link_to "My panel (#{current_user.email})", company_path(current_user)
+      link_to "My panel (#{current_company.email})", company_path(current_company)
     end
   end
 end
