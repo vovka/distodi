98b912f3010aee1e0a82d3a7348b5edf037ed9ae
Dmitry Chopey <dmitry.chopey@gmail.com>
2016-08-27 20:39:54 +0300

    Refactor User and add user pictures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,6 +73,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :phone)
+      params.require(:user).permit(:first_name, :last_name, :phone, :country, :city, :street, :postal_code, :notice, :picture)
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def logout_link
+    if user_signed_in?
+      link_to "Log out", destroy_user_session_path, method: :delete
+    elsif company_signed_in?
+      link_to "Log out", destroy_company_session_path, method: :delete
+    end
+  end
+
+  def my_panel
+    if user_signed_in?
+      link_to "My panel (#{current_user.email})", user_path(current_user)
+    elsif company_signed_in?
+      link_to "My panel (#{current_user.email})", company_path(current_user)
+    end
+  end
 end
