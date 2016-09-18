39e47825919e7171be1b8d6ff5a1cb2a3c7bfced
Dmitry Chopey <dmitry.chopey@gmail.com>
2016-09-18 15:22:41 +0300

    Front-end refactoring

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dchopey on 18.09.16.
+ */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
