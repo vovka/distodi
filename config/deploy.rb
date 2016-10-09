cb4280a6892b75c87f7e5054e4b836b45dddc0b0
Dmitry Chopey <dmitry.chopey@gmail.com>
2016-10-09 16:31:23 +0300

    Deplot config

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :format, :pretty
 set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, '.rbenv-vars'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
