d2c5ad24e31a109a4e7c378ad25dac032ae92dc9
Dmitry Chopey <dmitry.chopey@gmail.com>
2016-10-09 16:55:24 +0300

    Deplot config3

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,10 +24,10 @@ set :format, :pretty
 set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'shared/log', 'shared/pids', 'shared/sockets'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
