64aa5a50939c74dcc55f8a543cc312f23e488b33
Volodymyr Shcherbyna <sch.icq@gmail.com>
2017-04-10 14:41:23 +0300

    Set CET timezone. Added lib autoload. Configures dev env mailer.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Distodi
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'CET'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -33,5 +33,6 @@ module Distodi
         :authentication       => "plain",
         :enable_starttls_auto => true
     }
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
