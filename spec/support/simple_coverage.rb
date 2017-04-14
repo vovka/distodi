251537b4bd350cdebdee28cdb89c349df1ef37d6
Volodymyr Shcherbyna <sch.icq@gmail.com>
2017-04-14 03:32:01 +0300

    Fixed simplecov.

--- spec/support/simple_coverage.rb
+++ spec/support/simple_coverage.rb
@@ -2,14 +2,14 @@ if ENV['RAILS_ENV'] == 'test'
   require 'simplecov'
 
   SimpleCov.start do
-    add_filter "/spec/"
+    add_filter ["spec", "config"]
 
     add_group 'Controllers', 'app/controllers'
     add_group 'Helpers', 'app/helpers'
     add_group 'Mailers', 'app/mailers'
     add_group 'Models', 'app/models'
+    add_group 'Policies', 'app/policies'
     add_group 'Uploaders', 'app/uploaders'
-    add_group 'Views', 'app/views'
     add_group 'Libraries', 'lib'
   end
 end
