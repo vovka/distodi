8492744104b856c8ceea56f6c05e1bf6cc8d93a4
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2017-09-23 04:28:32 +0300

    Dealed with root path.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,11 @@ class StaticPagesController < ApplicationController
   layout "new", only: [:home]
 
   def home
+    if user_signed_in?
+      redirect_to dashboard_path
+    elsif company_signed_in?
+      redirect_to current_company
+    end
   end
 
   def tutorial
