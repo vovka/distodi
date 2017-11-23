8f92b8b6eb0dff6f38493d844fdeb1ebb08b78a4
Glib228 <Gleb_kh@bk.ru>
2017-11-23 18:02:10 +0200

    added new design of company profile
    fixed tests
    
    fixed my panel in helper

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -3,38 +3,38 @@ describe CompaniesController do
 
   describe "DELETE #destroy" do
     it "returns http success" do
-      company = FactoryGirl.create :company
+      company = FactoryGirl.create :company, password: "11111111", password_confirmation: "11111111"
       sign_in company
 
-      delete :destroy, id: company.to_param
+      delete :destroy, id: company.to_param, company: { password: "11111111" }
 
       expect(response).to redirect_to(root_path)
     end
 
     it "logs out company" do
-      company = FactoryGirl.create :company
+      company = FactoryGirl.create :company, password: "11111111", password_confirmation: "11111111"
       sign_in company
 
-      delete :destroy, id: company.to_param
+      delete :destroy, id: company.to_param, company: { password: "11111111" }
 
       expect(controller.current_company).to be_nil
     end
 
     it "defines an instance variable" do
-      company = create :company
+      company = create :company, password: "11111111", password_confirmation: "11111111"
       sign_in company
 
-      delete :destroy, id: company.to_param
+      delete :destroy, id: company.to_param, company: { password: "11111111" }
 
       expect(assigns(:company)).to be_present
     end
 
      it "should increment the count" do
-      company = create :company
+      company = create :company, password: "11111111", password_confirmation: "11111111"
       sign_in company
 
       expect {
-        delete :destroy, id: company.to_param
+        delete :destroy, id: company.to_param, company: { password: "11111111" }
       }.to change { Company.count }.from(1).to(0)
     end
   end
