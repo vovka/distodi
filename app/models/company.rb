06d3480cadc1bee0aa0c1bbc120ffe0bf0eeecd3
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2017-12-11 13:36:38 +0200

    Fixed items count

--- app/models/company.rb
+++ app/models/company.rb
@@ -8,6 +8,7 @@ class Company < ActiveRecord::Base
   URL_REGEXP = /\A(https?:\/\/)?(www\.)?[-a-zA-Z0-9._]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.,~#?!&\/=]*)\z/
 
   has_many :services
+  has_many :items, through: :services
   has_many :assigned_services, foreign_key: :approver_id,
                                class_name: "Service",
                                as: :approver
